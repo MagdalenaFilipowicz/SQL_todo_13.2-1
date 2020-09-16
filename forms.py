ed8e82faede5fe3df09e7901226c591a13952a70
Marta Kruszewska <martakruszewska007@gmail.com>
2020-09-16 20:15:57 +0200

    Fix checkobox

--- forms.py
+++ forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, optional
 
 
 class TodoForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
     description = TextField('desctrition', validators=[DataRequired()])
-    status = BooleanField('status', validators=[DataRequired()])
+    status = BooleanField('status', validators=[optional()])
+
+    
