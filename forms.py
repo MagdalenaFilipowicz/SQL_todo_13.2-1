aba4d8c2aa16b222daccc8f9da4acbecce28b531
Marta Kruszewska <martakruszewska007@gmail.com>
2020-09-16 20:31:29 +0200

    Fix check box

--- forms.py
+++ forms.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired, optional
 class TodoForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
     description = TextField('desctrition', validators=[DataRequired()])
-    status = BooleanField('status', validators=[optional()])
+    status = BooleanField('status')
 
     
