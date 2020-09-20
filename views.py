ce3d527c1d6c546f819f87e6d0368a94c9124cdc
Marta Kruszewska <martakruszewska007@gmail.com>
2020-09-20 18:01:26 +0200

    Add views

---
+++ views.py
@@ -0,0 +1,36 @@
+from flask import request, render_template, redirect, url_for
+from app import app
+from forms import TodoForm
+from models import todossqlite
+
+@app.route("/", methods=["GET", "POST"])
+def todos_list():
+    form = TodoForm() 
+    tasks = todossqlite.all()
+    error = ""
+    if request.method == "POST":
+        if form.validate_on_submit(): 
+            todossqlite.create()
+            task_id = todossqlite.add_task(form.data)
+            return redirect(url_for("todos_list"))
+
+    return render_template("todos.html", form=form, tasks=tasks, error=error)
+
+
+@app.route("/<int:todo_id>/", methods=["GET", "POST"])
+def todo_details(todo_id):
+    todo = todossqlite.get(todo_id)
+    data_todo = {}
+    data_todo['title'] = todo[0][1]
+    data_todo['description'] = todo[0][2]
+    data_todo['status'] = todo[0][3]
+    form_todo = TodoForm(data=data_todo)
+   
+    if request.method == "POST":
+        if request.form["btn"] == "Save":
+            todossqlite.update(todo_id=todo_id, data=form_todo.data)
+        if request.form["btn"] == "Delete":
+            todossqlite.delete(todo_id)
+        return redirect(url_for("todos_list"))
+
+    return render_template("todo.html", form=form_todo, todo_id=todo_id)
